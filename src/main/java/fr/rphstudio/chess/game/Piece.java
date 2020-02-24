ff84dd3f9bbe38309285e7e16a5cf8af5c57f1b7
Etienne Buronfosse <Etienne@MacBook-Pro-de-admin.local>
2020-02-24 13:45:45 +0100

    Création Pieces

---
+++ src/main/java/fr/rphstudio/chess/game/Piece.java
@@ -0,0 +1,11 @@
+package fr.rphstudio.chess.game;
+
+import fr.rphstudio.chess.interf.IChess;
+
+public class Piece {
+
+    private IChess.ChessType chessType;
+    private IChess.ChessColor chessColor;
+
+
+}
