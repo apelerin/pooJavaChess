9d789bfac0983474df5902bbe1c1027ee5f7632c
Axel P <arcanynka@gmail.com>
2020-02-24 12:09:51 +0100

    Fix of ChessModel to view the board for the first time

--- src/main/java/fr/rphstudio/chess/game/ChessModel.java
+++ src/main/java/fr/rphstudio/chess/game/ChessModel.java
@@ -4,6 +4,7 @@ import fr.rphstudio.chess.interf.EmptyCellException;
 import fr.rphstudio.chess.interf.IChess;
 import fr.rphstudio.chess.interf.OutOfBoardException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ChessModel implements IChess {
@@ -28,12 +29,12 @@ public class ChessModel implements IChess {
 
     @Override
     public ChessType getPieceType(ChessPosition p) throws EmptyCellException, OutOfBoardException {
-        return null;
+        return ChessType.TYP_PAWN;
     }
 
     @Override
     public ChessColor getPieceColor(ChessPosition p) throws EmptyCellException, OutOfBoardException {
-        return null;
+        return ChessColor.CLR_WHITE;
     }
 
     @Override
@@ -43,7 +44,7 @@ public class ChessModel implements IChess {
 
     @Override
     public List<ChessPosition> getPieceMoves(ChessPosition p) {
-        return null;
+        return new ArrayList<ChessPosition>();
     }
 
     @Override
@@ -53,12 +54,12 @@ public class ChessModel implements IChess {
 
     @Override
     public ChessKingState getKingState(ChessColor color) {
-        return null;
+        return ChessKingState.KING_SAFE;
     }
 
     @Override
     public List<ChessType> getRemovedPieces(ChessColor color) {
-        return null;
+        return new ArrayList<ChessType>();
     }
 
     @Override
