2255cca16025bbbf66e0b5faed40946ca0fec5e7
Axel P <arcanynka@gmail.com>
2020-02-28 02:37:26 +0100

    Create OneMove class, destined to trace the moves

---
+++ src/main/java/fr/rphstudio/chess/game/OneMove.java
@@ -0,0 +1,20 @@
+package fr.rphstudio.chess.game;
+
+import fr.rphstudio.chess.interf.IChess.*;
+
+/**
+ * Represent one move that has been done, it is meant to be contain in a list
+ */
+public class OneMove {
+    static int index = 0;
+
+    private ChessPosition pieceMoved;
+    private ChessPosition pieceMovedPos;
+    private boolean hasEaten;
+    private ChessPosition pieceEaten;
+    private ChessPosition pieceEatenPos;
+
+    public OneMove(int index) {
+        index++;
+    }
+}
